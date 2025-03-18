5d4b7b330fc5cb82c96109f935f4cd5572367255
CJ <cj@null.computer>
2025-03-18 12:32:34 -0600

    :sparkles: setup tailwind / daisy ui / icons (#48)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,8 @@ import antfu from "@antfu/eslint-config";
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
+// TODO: add tailwindcss plugin
+
 export default withNuxt(antfu({
   type: "app",
   vue: true,
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 });
