f83678c11e09f05b6e7f4d35a6e1323f797ec08b
CJ <cj@null.computer>
2025-03-18 11:27:46 -0600

    :sparkles: setup eslint / lint-staged (#47)

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import antfu from "@antfu/eslint-config";
+
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(antfu({
+  type: "app",
+  vue: true,
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+  ignores: [".pnpm-store/**"],
+}, {
+  rules: {
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
+}));
