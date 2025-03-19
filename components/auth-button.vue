61927ce1eb18239fd7c6ff57fe43920dd62d3765
w3cj <cj@null.computer>
2025-03-19 17:27:51 -0600

    :sparkles: add github oauth

---
+++ components/auth-button.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <button :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
+    Sign In With Github
+    <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
+    <Icon v-else name="tabler:brand-github" size="24" />
+  </button>
+</template>
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import db from "./db/index";
+import env from "./env";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -9,5 +10,11 @@ export const auth = betterAuth({
   }),
   advanced: {
     generateId: false,
+  },
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
   },
 });
