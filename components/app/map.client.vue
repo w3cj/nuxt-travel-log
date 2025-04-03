099e5c629c991917f337e31cd0c7db80626dee2a
w3cj <cj@null.computer>
2025-04-03 15:59:37 -0600

    :sparkles: add base map to dashboard

---
+++ components/app/map.client.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { CENTER_USA } from "~/lib/constants";
+
+const colorMode = useColorMode();
+const style = computed(() =>
+  colorMode.value === "dark"
+    ? "/styles/dark.json"
+    : "https://tiles.openfreemap.org/styles/liberty");
+const zoom = 3;
+</script>
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="CENTER_USA"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
