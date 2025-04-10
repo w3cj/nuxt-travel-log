6c86fb65fb87f2df72aa0bf37d407218e3469dc7
w3cj <cj@null.computer>
2025-04-09 22:34:55 -0600

    :sparkles: add location search

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -5,30 +5,13 @@ import slugify from "slug";
 import { findLocationByName, findUniqueSlug, insertLocation } from "~/lib/db/queries/location";
 import { InsertLocation } from "~/lib/db/schema";
 import defineAuthenticatedEventHandler from "~/utils/define-authenticated-event-handler";
+import sendZodError from "~/utils/send-zod-error";
 
 export default defineAuthenticatedEventHandler(async (event) => {
   const result = await readValidatedBody(event, InsertLocation.safeParse);
 
   if (!result.success) {
-    const statusMessage = result
-      .error
-      .issues
-      .map(issue => `${issue.path.join("")}: ${issue.message}`)
-      .join("; ");
-
-    const data = result
-      .error
-      .issues
-      .reduce((errors, issue) => {
-        errors[issue.path.join("")] = issue.message;
-        return errors;
-      }, {} as Record<string, string>);
-
-    return sendError(event, createError({
-      statusCode: 422,
-      statusMessage,
-      data,
-    }));
+    return sendZodError(event, result.error);
   }
 
   const existingLocation = await findLocationByName(result.data, event.context.user.id);
