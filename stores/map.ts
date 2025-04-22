03efa16fac43a4bf56d2934b7e2e9ef2286af727
w3cj <cj@null.computer>
2025-04-22 13:35:35 -0600

    :sparkles: show location logs in sidebar / map

--- stores/map.ts
+++ stores/map.ts
@@ -2,6 +2,8 @@ import type { LngLatBounds } from "maplibre-gl";
 
 import type { MapPoint } from "~/lib/types";
 
+import { CENTER_USA } from "~/lib/constants";
+
 export const useMapStore = defineStore("useMapStore", () => {
   const mapPoints = ref<MapPoint[]>([]);
   const selectedPoint = ref<MapPoint | null>(null);
@@ -18,8 +20,13 @@ export const useMapStore = defineStore("useMapStore", () => {
 
     effect(() => {
       const firstPoint = mapPoints.value[0];
-      if (!firstPoint)
+      if (!firstPoint) {
+        map.map?.flyTo({
+          center: CENTER_USA,
+          zoom: 2,
+        });
         return;
+      }
 
       bounds = mapPoints.value.reduce((bounds, point) => {
         return bounds.extend([point.long, point.lat]);
