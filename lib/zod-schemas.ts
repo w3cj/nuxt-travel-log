3165d669c69ce84c8ec96846acd9bba896117a69
w3cj <cj@null.computer>
2025-04-28 17:50:31 -0600

    :sparkles: a few fixes

--- lib/zod-schemas.ts
+++ lib/zod-schemas.ts
@@ -10,3 +10,6 @@ export const NameSchema = z.string().min(1).max(100);
 export const DescriptionSchema = z.string().max(1000).or(z.null());
 export const LatSchema = z.coerce.number().min(-90).max(90);
 export const LongSchema = z.coerce.number().min(-180).max(180);
+export const DateSchema = z.number({
+  message: "Date is required",
+});
