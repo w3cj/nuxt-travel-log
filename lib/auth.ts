b20492a7a01f28c6c4c445cabf45290945f867ce
Mihai <mihai.ppscu@gmail.com>
2025-05-01 12:33:17 +0300

    Update auth.ts
    learned some debugging skills along the way, but this is the correct way
    
    https://www.better-auth.com/docs/reference/options#advanced

--- lib/auth.ts
+++ lib/auth.ts
@@ -29,7 +29,9 @@ export const auth = betterAuth({
     provider: "sqlite",
   }),
   advanced: {
-    generateId: false,
+    database: {
+       generateId: false,
+    }
   },
   socialProviders: {
     github: {
